87b1ba2cf453a7c20bb8321f9bbb49b0fc15db78
gunwoongPark <bung1438@gmail.com>
2026-05-14 14:21:12 +0900

    feat: domain 변경

--- src/shared/config/site.ts
+++ src/shared/config/site.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "https://wow-raid-builder.vercel.app"
+export const SITE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "https://raid-scope.vercel.app"
 
 export const SITE_VERSION = "1.0.0"
 
